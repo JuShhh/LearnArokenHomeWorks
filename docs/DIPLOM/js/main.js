979f7de72a070da13573f71a1bc831b4f360b8a5
Petrova Julia <shapareva.ju@gmail.com>
2023-03-17 21:18:10 +0300

    ВЕРСТКА ДИПЛОМА ГОТОВА

--- docs/DIPLOM/js/main.js
+++ docs/DIPLOM/js/main.js
@@ -20,3 +20,30 @@ let swiper = new Swiper(document.querySelector(".swiper"), {
     delay: 5000,
   },
 });
+
+$(function () {
+  $(".faq__accordeon-link").on("click", function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("faq__accordeon-link--active")) {
+      $(this).removeClass("faq__accordeon-link--active");
+      $(this).children(".faq__accordeon-text").slideUp();
+    } else {
+      $(".faq__accordeon-link").removeClass("faq__accordeon-link--active");
+      $(".faq__accordeon-text").slideUp();
+
+      $(this).addClass("faq__accordeon-link--active");
+      $(this).children(".faq__accordeon-text").slideDown();
+    }
+  });
+});
+
+$(function () {
+  $(
+    ".header__nav-list a, .header__buttons-contact, .header__info-btn, .visit__btn, .footer__logo, .footer__btn-blue, .footer__nav-link"
+  ).on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top + 100;
+    $("body,html").animate({ scrollTop: top }, 1700);
+  });
+});
